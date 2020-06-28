117a129d95dece239ea022a571807c7232e8ffbc
patrykash <patryksieczka4@gmail.com>
2020-06-28 13:32:10 +0200

    Add equals and hashCode method to ResultRank

--- src/main/java/methods/ResultRank.java
+++ src/main/java/methods/ResultRank.java
@@ -23,4 +23,25 @@ public class ResultRank implements Comparable<ResultRank> {
     public int compareTo(ResultRank o) {
         return Double.compare(this.rating, o.getRating());
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ResultRank)) return false;
+
+        ResultRank that = (ResultRank) o;
+
+        if (Double.compare(that.rating, rating) != 0) return false;
+        return variantName.equals(that.variantName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = variantName.hashCode();
+        temp = Double.doubleToLongBits(rating);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
