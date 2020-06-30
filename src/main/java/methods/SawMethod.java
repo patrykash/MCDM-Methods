2754adecdea9f92bd357107adfcb95e448ebe55d
patrykash <patryksieczka4@gmail.com>
2020-06-30 11:10:12 +0200

    Add static factory to SawMethod

--- src/main/java/methods/SawMethod.java
+++ src/main/java/methods/SawMethod.java
@@ -8,21 +8,32 @@ import java.util.List;
 public class SawMethod implements McdmMethod{
     private DecisionProblem decisionProblem;
 
-    public SawMethod(DecisionProblem decisionProblem) {
+    private MatrixNormalization matrixNormalization;
+
+
+    private SawMethod(DecisionProblem decisionProblem, MatrixNormalization matrixNormalization) {
         this.decisionProblem = decisionProblem;
+        this.matrixNormalization = matrixNormalization;
     }
 
     public List<ResultRank> calculateResult() throws ValidationException {
         Validator validator = new Validator();
         validator.validate(decisionProblem);
-        MaxMinNormalization maxMinNormalization = new MaxMinNormalization();
-        maxMinNormalization.setDecisionProblem(decisionProblem);
-        double[][] normalizedMatrix = maxMinNormalization.normalizeMatrix();
+        matrixNormalization.setDecisionProblem(decisionProblem);
+        double[][] normalizedMatrix = matrixNormalization.normalizeMatrix();
 
         double[] rating = MatrixOperation.multiplyMatrixByVector(normalizedMatrix, decisionProblem.getWeightsVector());
 
         return Util.assignRatingsToVariants(rating, decisionProblem.getVariants());
 
+    }
+
+    public static SawMethod createWithMaxMinNormalization(DecisionProblem decisionProblem) {
+        return new SawMethod(decisionProblem, new MaxMinNormalization());
+    }
+
+    public static SawMethod createWithVectorNormalization(DecisionProblem decisionProblem) {
+        return new SawMethod(decisionProblem, new VectorNormalization());
     }
 
 
